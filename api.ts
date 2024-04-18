cc30697ab18a524a9c7d23dca31b86e1df196df9
Rail Khusnutdinov <rail@33.run>
2024-04-18 11:41:27 +0300

    simplify errors handle

--- api.ts
+++ api.ts
@@ -1,92 +1,64 @@
 import { apiUrl, isTesting } from "./env";
 
 export const URI = apiUrl + "/api/";
-export const CLIENT_ERRORS = [400, 401, 403, 404];
+export const CLIENT_ERROR = 400;
 
 interface UserMessage {
   text: string;
   field: string | null;
 }
 
-/*
-вопрос: нужен ли код для сообщения об ошибке? может достаточно текстового сообщения?
-с одной стороны хорошо отправлять текстовые сообщения для того чтобы зайти в консоль разработчика
-и увидеть причину ошибки, не сопоставляя номер ошибки с текстом
-с другой стороны номер ошибки можно отправлять в статистику
-еще можно создать перечисления с номером ошибки и не работать с текстом в коде.
-а еще ошибки могут повторяться, то есть за один запрос может быть несколько ошибок
-*/
-interface ErrorType {
-  // текстовый ид ошибки
-  message: string;
-  // сообщение для клиента, если присутствует, показывать клиенту.
-  userMessage: UserMessage | null;
-}
+// const enum BodyStatus {
+//   Success = "Success",
+//   Error = "Error",
+// }
 
-const enum BodyStatus {
-  Success = "Success",
-  Error = "Error",
-}
+// interface BodySuccess<D = any> {
+//   status: BodyStatus.Success;
+//   data: D;
+// }
 
-interface BodySuccess<D = any> {
-  status: BodyStatus.Success;
-  data: D; // данные для приложения
-}
+// interface ClientError {
+//   status: BodyStatus.Error;
+//   error: { client: string };
+// }
 
-interface BodyError {
-  status: BodyStatus.Error;
-  errors: ErrorType[]; // описание ошибки для логов
-}
+// interface UserError {
+//   status: BodyStatus.Error;
+//   error: { user: UserMessage };
+// }
 
-type BodyType<D> = BodySuccess<D> | BodyError;
+// type BodyType<D> = BodySuccess<D> | UserError;
 
-// type Body<D> = BodyType<D> & {
-//   unwrap(): D
-// };
-
-class ResBody<D = any> {
-  status: BodyStatus;
-  data?: D;
-  errors?: ErrorType[];
-
-  constructor(body: BodyType<D>) {
-    this.status = body.status;
-
-    if (body.status === BodyStatus.Success) {
-      this.data = body.data;
-    } else {
-      this.errors = body.errors;
-    }
-  }
-
-  unwrap(this: this): D {
-    if (this.status === BodyStatus.Success) {
-      return this.data as D;
-    } else {
-      throw new EntityError(this.errors ?? []);
-    }
-  }
-}
+// class ResBody<D = any> {
+//   unwrap(this: this): D {
+//     if (this.status === BodyStatus.Success) {
+//       return this.data as D;
+//     } else {
+//       throw new EntityError(this.error);
+//     }
+//   }
+// }
 
 export class ApiError extends Error {
   statusCode: number;
-  messages: string[];
+  message: string;
 
-  constructor(statusCode: number, messages: string[]) {
-    super(messages.join("\n"));
+  constructor(statusCode: number, message: string) {
+    super(message);
     this.name = "ApiError";
     this.statusCode = statusCode;
-    this.messages = messages;
+    this.message = message;
   }
 }
 
 export class EntityError extends Error {
-  errors: ErrorType[];
+  error: UserMessage;
 
-  constructor(errors: ErrorType[]) {
+  constructor(error: UserMessage) {
     super("entity error");
     this.name = "EntityError";
-    this.errors = errors;
+    this.error = error;
   }
 }
 
@@ -107,23 +79,23 @@ export interface RequestParams {
   method: Method;
   requiredSignin?: closure;
   metricSend?: (url: string, ms: number) => void;
-  errorSend?: (statusCode: number, name: string, message: string) => void;
   timeout?: number;
 }
 
 /**
  * Все запросы могут вернуть либо ОК,
  * либо Unauthorized 401 - необходимо перенаправить на страницу авторизации?
- * или вывести сообщение о необходимости авторизации
- * либо 404 - "Не найдено..."
- * либо Server error 500 - необходимо вывести ошибку "Ошибка..."
- * либо Bad request 400 - необходимо вывести ошибку "Ошибка..."
- * либо 403 - "Ошибка..."
- * Либо свою кастомную ошибку - в этом случае необходимо вывести эту кастомную ошибку
- * Кастомная ошибка отдается как 200, но у нее есть свой номер ошибки, сообщение для консоли и сообщение для пользователя
- * Кастомные ошибки должны обрабатываться в компоненте, в котором делается запрос. В теле промиса в then, а не catch.
+ *
+ * - или вывести сообщение о необходимости авторизации
+ * - либо 404 - "Не найдено..."
+ * - либо Server error 500 - необходимо вывести ошибку "Ошибка..."
+ * - либо Bad request 400 - необходимо вывести ошибку "Ошибка..."
+ * - либо 403 - "Ошибка..."
+ * - Либо свою кастомную ошибку - в этом случае необходимо вывести эту кастомную ошибку
+ * - Кастомная ошибка отдается как 200, но у нее есть свой номер ошибки, сообщение для консоли и сообщение для пользователя
+ * - Кастомные ошибки должны обрабатываться в компоненте, в котором делается запрос. В теле промиса в then, а не catch.
  * */
-export const request = async <D>(data: RequestParams): Promise<ResBody<D>> => {
+export const request = async <D>(data: RequestParams): Promise<D> => {
   const { params, path, body, method, requiredSignin, metricSend } = data;
   let uri = path;
 
@@ -188,48 +160,45 @@ export const request = async <D>(data: RequestParams): Promise<ResBody<D>> => {
   const isJson = contentType === "application/json";
 
   if (res.ok) {
-    let body: BodyType<D> = {
-      status: BodyStatus.Success,
-      data: null as D,
-    };
+    let body: D = null as D;
 
     if (isJson) {
       body = await res.json();
     } else if (contentType === "text/plain") {
       const data = (await res.text()) as D;
-      body.data = data;
+      body = data;
     } else if (contentType === "application/octet-stream") {
       const buffer = await res.arrayBuffer();
       const data = new DataView(buffer) as D;
-      body.data = data;
+      body = data;
     }
 
     const ms = performance.now() - time;
     metricSend?.(uri, ms);
 
-    return new ResBody(body);
+    return body;
   }
 
   // Unprocessable Entity
   if (res.status === 422 && isJson) {
-    const json: BodyType<D> = await res.json();
+    const json: UserMessage = await res.json();
     const ms = performance.now() - time;
     metricSend?.(uri, ms);
 
-    return new ResBody(json);
+    throw new EntityError(json);
   }
 
-  let messages = [] as string[];
+  let message = "";
 
-  if (CLIENT_ERRORS.includes(res.status) && isJson) {
-    const json: BodyError = await res.json();
-    messages = json.errors.map((e) => e.message);
+  if (res.status === CLIENT_ERROR && isJson) {
+    message = await res.text();
   }
 
   const ms = performance.now() - time;
   metricSend?.(uri, ms);
 
-  // const isClientError = [400, 401, 403, 404].includes(res.status);
+  // export const CLIENT_ERRORS = [400, 401, 403, 404];
+  // const isClientError = CLIENT_ERRORS.includes(res.status);
   // if (!isClientError) {
   //   const text = await res.text();
   //   errorSend?.(res.status, "api_error", text);
@@ -239,15 +208,5 @@ export const request = async <D>(data: RequestParams): Promise<ResBody<D>> => {
     requiredSignin?.();
   }
 
-  throw new ApiError(res.status, messages);
-
-  // return new ResBody({
-  //   status: BodyStatus.Error,
-  //   errors: [
-  //     {
-  //       message: res.statusText,
-  //       userMessage: null,
-  //     },
-  //   ],
-  // });
+  throw new ApiError(res.status, message);
 };
